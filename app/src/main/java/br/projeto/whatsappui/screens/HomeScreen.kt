a04bd43abb5cb20f43d137bd3f45bc142457366e
rafsn <santos_rafa1973@hotmail.com>
2024-10-07 23:08:43 -0300

    Initial commit

---
+++ app/src/main/java/br/projeto/whatsappui/screens/HomeScreen.kt
@@ -0,0 +1,18 @@
+package br.projeto.whatsappui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun HomeScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun HomeScreenPrev() {
+    HomeScreen()
+}
